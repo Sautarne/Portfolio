66eda989f9c5a700fe8b71d23be7b71fcadb2fc5
Christophe PEREZ <christophe.perez690@gmail.com>
2023-11-05 20:27:33 +0100

    save 05-11-2023

---
+++ src/components/Contenu.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div class="container">
+
+    <p>Une modale en Vue JS</p>
+
+    <modale v-bind:revele='revele' v-bind:toggleModale='toggleModale'></modale>
+    <button class="btn-toggle" v-on:click='toggleModale' >Ouvre la modale</button>
+
+    </div>
+
+</template>
+
+
+<script>
+    
+    import Modale from './Modale.vue'
+
+    export default {
+        name: 'Contenu',
+        data(){
+            return{
+                revele: false
+            }
+        },
+        components: {
+            'modale': Modale
+        },
+        methods: {
+            toggleModale: function(){
+                this.revele = !this.revele
+            }
+        }
+    }
+
+</script>
+
+
+<style>
+.container p{
+    color: aqua;
+}
+</style>
---
+++ src/components/Modale.vue
@@ -0,0 +1,66 @@
+<template>
+
+<div class="bloc-modale" v-if='revele'>
+
+    <div class="overlay" v-on:click="toggleModale"></div>
+    <div class="modale">
+        <button class="btn-modale" v-on:click="toggleModale">X</button>
+        <p>Le contenu de la modale</p>
+
+    </div>
+
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+    name: 'modale',
+    props: ['revele', 'toggleModale']
+}
+
+</script>
+
+
+<style scoped>
+
+.bloc-modale {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.overlay{
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
+
+.modale{
+    background: #f1f1f1;
+    color: #333;
+    padding: 50px;
+    position: fixed;
+    top: 30%;
+}
+
+.btn-modale{
+    position: absolute;
+    top: 10px;
+    right: 10px;
+}
+.bloc-modale p{
+    color: red;
+}
+
+</style>
